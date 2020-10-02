85d0eb8c1028c46bbdd962d980482d6c1e146e79
ilixistorres <62942127+ilixistorres@users.noreply.github.com>
2020-10-03 03:51:50 +0800

    Upload OccMin three scaled sites with minimap

---
+++ Scaled/pointclouds/Ilin-Ambulong/cloud.js
@@ -0,0 +1,30 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+	"projection": "+proj=somerc +lat_0=12.230617 +lon_0=121.032856 +k_0=1 +x_0=0 +y_0=0 +ellps=bessel +towgs84=674.4,15.1,405.3,0,0,0,0 +units=m +no_defs",
+    "points": 17233308,
+    "boundingBox": {
+        "lx": -388.405803,
+        "ly": -2368.030794,
+        "lz": -124.272484,
+        "ux": 4136.601033,
+        "uy": 2156.976041,
+        "uz": 4400.734352
+    },
+    "tightBoundingBox": {
+        "lx": -295.582571,
+        "ly": -2027.746251,
+        "lz": -113.632484,
+        "ux": 799.695906,
+        "uy": 1527.236955,
+        "uz": 55.385596
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED",
+        "CLASSIFICATION"
+    ],
+    "spacing": 35.351616,
+    "scale": 0.010000,
+    "hierarchyStepSize": 5
+}
