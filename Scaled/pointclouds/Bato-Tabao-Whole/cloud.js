d4a08f88229d5e133e8fcc0dfefd3c8df7f35481
ilixistorres <62942127+ilixistorres@users.noreply.github.com>
2020-10-06 05:36:53 +0800

    Added whole Bato Tabao 3D
    gfdthr

---
+++ Scaled/pointclouds/Bato-Tabao-Whole/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "points": 118139151,
+    "boundingBox": {
+        "lx": -1260.452751,
+        "ly": -892.921651,
+        "lz": -425.145485,
+        "ux": 1715.067268,
+        "uy": 2082.598368,
+        "uz": 2550.374534
+    },
+    "tightBoundingBox": {
+        "lx": -1069.812751,
+        "ly": -720.886651,
+        "lz": -379.755485,
+        "ux": 1038.599764,
+        "uy": 1185.904612,
+        "uz": 153.914518
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED",
+        "CLASSIFICATION"
+    ],
+    "spacing": 23.246250,
+    "scale": 0.010000,
+    "hierarchyStepSize": 5
+}
