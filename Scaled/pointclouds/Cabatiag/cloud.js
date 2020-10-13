d3033e322d6dcfdf9a8a7c31cca113be25586731
ilixistorres <62942127+ilixistorres@users.noreply.github.com>
2020-10-13 20:37:19 +0800

    Added Carawisan Shallow and Cabatiag

---
+++ Scaled/pointclouds/Cabatiag/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "points": 112427121,
+    "boundingBox": {
+        "lx": -256.489766,
+        "ly": 396.536790,
+        "lz": -525.250591,
+        "ux": 2156.017070,
+        "uy": 2809.043626,
+        "uz": 1887.256245
+    },
+    "tightBoundingBox": {
+        "lx": -112.295798,
+        "ly": 585.623887,
+        "lz": -457.160591,
+        "ux": 1497.567232,
+        "uy": 2542.217982,
+        "uz": 252.176118
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED",
+        "CLASSIFICATION"
+    ],
+    "spacing": 18.847710,
+    "scale": 0.010000,
+    "hierarchyStepSize": 5
+}
