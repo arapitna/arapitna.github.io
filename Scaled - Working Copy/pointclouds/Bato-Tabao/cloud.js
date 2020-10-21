5b79fe0a0607b324f6f476bee6d6f41b2de630f0
ilixistorres <62942127+ilixistorres@users.noreply.github.com>
2020-10-21 14:57:35 +0800

    Backed up WORKING COPY

---
+++ Scaled - Working Copy/pointclouds/Bato-Tabao/cloud.js
@@ -0,0 +1,30 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+	"projection": "+proj=somerc +lat_0=12.632895 +lon_0=120.902848 +k_0=1 +x_0=0 +y_0=0 +ellps=bessel +towgs84=674.4,15.1,405.3,0,0,0,0 +units=m +no_defs",
+    "points": 118139151,
+    "boundingBox": {
+        "lx": -1260.452751,
+        "ly": -892.921651,
+        "lz": -425.145485,
+        "ux": 1715.067268,
+        "uy": 2082.598368,
+        "uz": 2550.374534
+    },
+    "tightBoundingBox": {
+        "lx": -1069.812751,
+        "ly": -720.886651,
+        "lz": -379.755485,
+        "ux": 1038.599764,
+        "uy": 1185.904612,
+        "uz": 153.914518
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED",
+        "CLASSIFICATION"
+    ],
+    "spacing": 23.246250,
+    "scale": 0.010000,
+    "hierarchyStepSize": 5
+}
