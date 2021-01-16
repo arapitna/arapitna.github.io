d2364b10a29507095da087414b6ca3a97532284b
ilixistorres <62942127+ilixistorres@users.noreply.github.com>
2021-01-17 05:32:15 +0800

    deleted occmin folder
    wrong rescaling so reuploading everything to omppdo

---
+++ omppdo/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "points": 17233308,
+    "boundingBox": {
+        "lx": -585.331183,
+        "ly": -3568.641781,
+        "lz": -187.279849,
+        "ux": 6233.889031,
+        "uy": 3250.578434,
+        "uz": 6631.940366
+    },
+    "tightBoundingBox": {
+        "lx": -445.531183,
+        "ly": -3055.865360,
+        "lz": -171.279849,
+        "ux": 1205.073870,
+        "uy": 2301.525591,
+        "uz": 83.420783
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED",
+        "CLASSIFICATION"
+    ],
+    "spacing": 53.275158,
+    "scale": 0.100000,
+    "hierarchyStepSize": 5
+}
