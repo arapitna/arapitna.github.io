d71df1550e92ed4c4bae106cfdd2bcf9596e86f4
ilixistorres <62942127+ilixistorres@users.noreply.github.com>
2021-01-17 07:36:49 +0800

    Uploaded Carawisan Deep pc
    But I might have mistakenly named this and should be Bato-Tabao instead

---
+++ omppdo/pointclouds/Carawisan-Deep/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "points": 145885681,
+    "boundingBox": {
+        "lx": -1476.850717,
+        "ly": -1374.660858,
+        "lz": -643.809018,
+        "ux": 2812.411002,
+        "uy": 2914.600861,
+        "uz": 3645.452701
+    },
+    "tightBoundingBox": {
+        "lx": -1222.151716,
+        "ly": -1106.552000,
+        "lz": -571.739303,
+        "ux": 1433.970143,
+        "uy": 1846.825431,
+        "uz": 259.528697
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED",
+        "CLASSIFICATION"
+    ],
+    "spacing": 33.509857,
+    "scale": 0.010000,
+    "hierarchyStepSize": 5
+}
