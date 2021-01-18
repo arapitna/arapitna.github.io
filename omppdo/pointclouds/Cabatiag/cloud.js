7c100b266aa92442add1155b55c7bdb41327d4aa
ilixistorres <62942127+ilixistorres@users.noreply.github.com>
2021-01-18 16:10:04 +0800

    uploaded all the point clouds

---
+++ omppdo/pointclouds/Cabatiag/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "points": 112461581,
+    "boundingBox": {
+        "lx": -5006.340049,
+        "ly": 7739.869748,
+        "lz": -10454.528465,
+        "ux": 42082.585733,
+        "uy": 54828.795529,
+        "uz": 36634.397316
+    },
+    "tightBoundingBox": {
+        "lx": -2191.764420,
+        "ly": 11271.809842,
+        "lz": -9350.581767,
+        "ux": 29230.625357,
+        "uy": 49620.679806,
+        "uz": 4922.143074
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED",
+        "CLASSIFICATION"
+    ],
+    "spacing": 367.882233,
+    "scale": 0.100000,
+    "hierarchyStepSize": 5
+}
---
+++ omppdo/pointclouds/Carawisan-Shallow/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "points": 62149391,
+    "boundingBox": {
+        "lx": -407.812970,
+        "ly": -987.446104,
+        "lz": -259.938013,
+        "ux": 2502.818133,
+        "uy": 1923.184999,
+        "uz": 2650.693090
+    },
+    "tightBoundingBox": {
+        "lx": -275.906443,
+        "ly": -860.089577,
+        "lz": -228.088013,
+        "ux": 1247.937026,
+        "uy": 611.835280,
+        "uz": 176.028791
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED",
+        "CLASSIFICATION"
+    ],
+    "spacing": 22.739305,
+    "scale": 0.010000,
+    "hierarchyStepSize": 5
+}
